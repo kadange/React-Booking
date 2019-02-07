33208b69cd9e55fa2119ba2d3ecabcf2728b4d3f
Gene Anthony Kadano <gene.anthony.kadano@oocl.com>
2019-02-07 17:36:56 +0800

    wrap in Form

--- src/component/ManageBooking.js
+++ src/component/ManageBooking.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import CityList from '../container/CityContainer';
 import ContainerComponent from './ContainerComponent';
 import {Button, Divider, Input, Modal, Form, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, AutoComplete} from 'antd';
@@ -7,28 +7,67 @@ import {Button, Divider, Input, Modal, Form, Tooltip, Icon, Cascader, Select, Ro
 const confirm = Modal.confirm;
 
 const ManageBooking = () => {
+    const formItemLayout = {
+        labelCol: {
+            xs: { span: 24 },
+            sm: { span: 2 },
+        },
+        wrapperCol: {
+            xs: { span: 16 },
+            sm: { span: 4 },
+        },
+    };
+    const tailFormItemLayout = {
+        wrapperCol: {
+            xs: {
+                span: 24,
+                offset: 2,
+            },
+            sm: {
+                span: 16,
+                offset: 1,
+            },
+        },
+    };
+
     function showConfirm() {
         confirm({
-          title: 'Do you want to delete these items?',
-          content: 'When clicked the OK button, this dialog will be closed after 1 second',
-          onOk() {
-            return new Promise((resolve, reject) => {
-              setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
-            }).catch(() => console.log('Oops errors!'));
-          },
-          onCancel() {},
+            title: 'Do you want to delete these items?',
+            content: 'When clicked the OK button, this dialog will be closed after 1 second',
+            onOk() {
+                return new Promise((resolve, reject) => {
+                setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
+                }).catch(() => console.log('Oops errors!'));
+            },
+            onCancel() {},
         });
     }
 
     return (
         <div style={{ borderStyle: "groove", padding: "10px"}}>
-            <CityList componentName="From City" name="fromCity" />
-            <CityList componentName="To City" name="toCity" />
-            <Divider orientation="left" >Container Details:</Divider>
-                        
-            <label>Description: </label><Input placeholder="Cargo Description" allowClear />
-            <Button type="default" onClick={showConfirm}>Add</Button><Button type="default">Delete</Button>
-            <ContainerComponent />
+            <Form>
+                <Form.Item {...formItemLayout}
+                    label="From City">
+                    <CityList name="fromCity" />
+                </Form.Item>
+                <Form.Item {...formItemLayout}
+                    label="To City">
+                    <CityList name="toCity" />
+                </Form.Item>
+                
+                <Divider orientation="left" >Container Details:</Divider>
+                
+                <Form.Item {...formItemLayout}
+                    label="Description">
+                    <Input placeholder="Cargo Description" allowClear />
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout} >
+                    <Button type="default" onClick={showConfirm}>Add</Button>
+                    <Button type="default">Delete</Button>
+                </Form.Item>
+                
+                <ContainerComponent />
+            </Form>
         </div>
     );
 }
