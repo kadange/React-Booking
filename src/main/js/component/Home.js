8af2f76988464940d1a6ba6b606e404677b52ca5
Gene Anthony Kadano <gene.anthony.kadano@oocl.com>
2019-02-13 10:57:40 +0800

    Add component for Home and Search

---
+++ src/main/js/component/Home.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet, eget nonummy egestas est facilisis leo. 
+                    Vestibulum pulvinar facilisis vestibulum urna tortor varius. Id a sed nec eleifend,  
+                    sed nonummy in, ligula ipsum. Amet dui cursus sit elit, cupiditate eleifend tortor cras a sodales vitae, 
+                    purus massa lorem, tellus vestibulum pulvinar nunc sollicitudin nam, non gravida amet. Leo mauris enim nam. 
+                    Sociis dictumst, nec malesuada, etiam in egestas placerat quis nec, praesent est velit proin mi massa eligendi. 
+                    Tempor pellentesque adipiscing ac dictumst lectus sed, cursus praesent elit, nisl dictumst curabitur urna eget nisl elit, 
+                    enim nam, mollis at molestie quam nunc et. Vestibulum per dolore convallis porta nec arcu, eros auctor pulvinar amet parturient non, 
+                    suscipit sodales netus eu. Potenti wisi labore, ligula curabitur vel nibh cum ea elit. 
+                    <br/> <br/>
+                    Libero ante quis per. Parturient amet luctus purus. Tortor curabitur nunc non mauris augue vel, dui suspendisse eros felis, 
+                    lorem et turpis vestibulum diam, natoque ultricies nullam mi leo wisi dui. Potenti scelerisque vestibulum tristique. 
+                    Cursus mattis ut habitant, eget egestas in tempus vehicula orci. Elementum nullam. Suspendisse tempor ante, 
+                    porta elit turpis turpis, donec placerat dolor sagittis, porttitor a mattis omnis sociis molestie, vulputate tortor duis nostrum. 
+                    Accusantium integer vestibulum tempor scelerisque, a sit molestie elit nulla mauris, lobortis sit varius ut aliquam neque conubia. 
+                    Dignissim gravida mattis pede. Magna sem ac, fusce commodo sapien sollicitudin purus, aliquet erat sed amet tellus pharetra vitae. 
+                    Consectetuer morbi consequat, facilisis et id. Taciti suscipit in id, nunc praesent ornare, vitae integer commodo, duis elit, ipsum suscipit natoque. 
+                    Torquent curabitur ut suspendisse blandit, morbi id do. Gravida etiam maecenas, etiam ridiculus viverra cursus. Nec mauris. 
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Home;
