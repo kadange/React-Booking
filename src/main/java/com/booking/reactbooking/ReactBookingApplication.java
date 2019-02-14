f1a73041822ee2e595d03b4a534e152ceeb4385f
Gene Anthony Kadano <gene.anthony.kadano@oocl.com>
2019-02-14 10:08:55 +0800

    add initial java

---
+++ src/main/java/com/booking/reactbooking/ReactBookingApplication.java
@@ -0,0 +1,14 @@
+package com.booking.reactbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBookingApplication.class, args);
+	}
+
+}
+
